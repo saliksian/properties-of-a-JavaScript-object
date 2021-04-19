8df4dc7f7f1382766a9525686558f793115c9b14
Muhammad Salik <75317422+saliksian@users.noreply.github.com>
2021-04-20 04:52:01 +0500

    final commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var student = {
+    name : "David Rayy",
+    sclass : "VI",
+    rollno : 12 };
+
+    for(var x in student){ // here I use for in loop to display object properties.
+        // You have the the basic knowledge of for in loop to understand this little program 
+        console.log(x);
+    }
+
+
